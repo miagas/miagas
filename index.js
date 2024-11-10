f3eaee24d4ebe69a6fb4585c7cca18f778cc55dd
harryto1 <harry.ruiz6@upr.edu>
2024-11-10 04:42:14 -0400

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const pedo1 = new Audio ('/assets/pedo1.mp4');
 const boton1 = document.getElementById('pedo1');
 boton1.addEventListener('click', () => {
+    pedo1.load();
     pedo1.play();
 });
 
