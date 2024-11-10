369c899210999cde13d690923c4da739f0f93d65
harryto1 <harry.ruiz6@upr.edu>
2024-11-10 05:05:07 -0400

    pedo 2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let pedo1 = document.getElementById('audio1')
 const boton1 = document.getElementById('pedo1');
+let pedo2 = document.getElementById('audio2')
+const boton2 = document.getElementById('pedo2');
 boton1.addEventListener('click', () => {
     pedo1.play();
 });
-
+boton2.addEventListener('click', () => {
+    pedo2.play();
+});
