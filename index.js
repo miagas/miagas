73623a7bb25cf1acde4739c5f12e0bdebd7648e3
harryto1 <harry.ruiz6@upr.edu>
2024-11-10 04:53:04 -0400

    bug fix 3

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let pedo1 = document.getElementById('audio1')
 const boton1 = document.getElementById('pedo1');
 boton1.addEventListener('click', () => {
-    pedo1.load();
     pedo1.play();
 });
 
